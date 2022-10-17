d6ec49266653340a382232bb35c907d339cca7f9
Shridhar Karigoudar <shree277004@gmail.com>
2022-10-17 17:01:24 +0530

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello this is Node App running on Azure with continous integration and continous deployment..! \nAuthor is MR. Shridhar Karigoudar'
+  const msg = 'Hello this is Node App running on Azure with continous integration and continous deployment(Devops Ideology)! \nAuthor is MR. Shridhar Karigoudar \n\n\n\n\n                                                            Thanks for visiting the build'
   res.end(msg);
 });
 
