0cc19084df0c6072a6fc53e399fdd75334b1a91d
Shridhar Karigoudar <shree277004@gmail.com>
2022-10-17 17:58:36 +0530

    added eslint

--- index.js
+++ index.js
@@ -5,22 +5,16 @@ let date_ob = new Date();
 // current date
 // adjust 0 before single digit date
 let date = ("0" + date_ob.getDate()).slice(-2);
-
 // current month
 let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-
 // current year
 let year = date_ob.getFullYear();
-
 // current hours
 let hours = date_ob.getHours();
-
 // current minutes
 let minutes = date_ob.getMinutes();
-
 // current seconds
 let seconds = date_ob.getSeconds();
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello this is Node App running on Azure with continous integration and continous deployment(Devops Ideology)! \nAuthor is MR. Shridhar Karigoudar \n\n\n\n\n                                                       Thanks for visiting my First build and visiting date and time is '+year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds
