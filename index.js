1351cde101f98345f590b6113c59574192fc993d
Shridhar Karigoudar <shree277004@gmail.com>
2022-10-17 16:48:41 +0530

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! This is rc\n'
+  const msg = 'Hello this is Node App...! \n author is MR. Shridhar Karigoudar'
   res.end(msg);
 });
 
